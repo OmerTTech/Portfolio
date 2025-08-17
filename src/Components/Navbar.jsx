41751a89afc9eb4bfe6df7656d45638c704eed36
HyperOmerJ <omer.tapdiqov@gmail.com>
2025-08-18 01:46:08 +0400

    Navbar finished, then starting header

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { IoMoonOutline } from "react-icons/io5";
 import profile from "../Assets/profile.png";
+import { RiMoonLine } from "react-icons/ri";
 
 const Navbar = () => {
   return (
-    <nav className="w-full flex items-center justify-between py-2 font-[900] tracking-wider">
-      <div className="flex items-center text-lg">
-        <img src={profile} width={50} height={50} />
+    <nav className="w-full flex items-center justify-between py-4 text-sm font-[500]">
+      <div className="flex items-center text-[15px] tracking-widest cursor-pointer">
+        <img src={profile} width={30} height={30} />
         merTTech
       </div>
       <div>
-        <ul className="flex items-center gap-5 *:p-2">
+        <ul className="flex items-center gap-5 *:p-2 text-[13.5px] tracking-wide *:*:hover:text-violet-600">
           <a href="#">
-            <li className="text-sky-600">Home</li>
+            <li className="text-violet-600">Home</li>
           </a>
           <a href="#">
             <li>About</li>
@@ -23,11 +23,11 @@ const Navbar = () => {
           <a href="#">
             <li>Portfolio</li>
           </a>
-          <a href="#">
+          <a href="#" className="!pe-0">
             <li>Contact Me</li>
           </a>
-          <li className="text-2xl">
-            <IoMoonOutline />
+          <li className="text-xl !p-0  hover:text-violet-600 transition-all cursor-pointer">
+            <RiMoonLine />
           </li>
         </ul>
       </div>
