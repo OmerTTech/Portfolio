765d703b7cf46aa087ac8ce1e353e196dc6d3ca7
HyperOmerJ <omer.tapdiqov@gmail.com>
2025-08-21 20:38:43 +0400

    mobile responsive bugs fixed

--- src/Components/Mobile/MobileNavbar.jsx
+++ src/Components/Mobile/MobileNavbar.jsx
@@ -32,9 +32,9 @@ const MobileNavbar = () => {
   }, []);
 
   return (
-    <nav className="max-w-[1000px] w-full flex sm:hidden items-center justify-between gap-20 ps-5 pe-4 py-4  text-sm font-[500] bg-white">
+    <nav className="max-w-[1000px] w-full flex sm:hidden items-center justify-between gap-20 ps-8 pe-4 py-6  text-sm font-[500] bg-white">
       <ScrollLink to="home">
-        <div className="flex items-center text-[15px] tracking-widest cursor-pointer !text-black">
+        <div className="flex items-center text-[15px] tracking-widest scale-110 cursor-pointer !text-black">
           <img
             src={profile}
             className="hover:contrast-125 hover:brightness-95 drop-shadow-black drop-shadow-2xs rounded-full"
@@ -45,11 +45,11 @@ const MobileNavbar = () => {
         </div>
       </ScrollLink>
       <div>
-        <ul className="flex items-center gap-4 *:px-3 *:py-0 text-[13.5px] tracking-wide *:hover:*:text-hover *:cursor-pointer">
-          <li className="text-xl !p-0 hover:text-main transition">
+        <ul className="flex items-center gap-4 *:px-5 *:py-0 text-[13.5px] tracking-wide *:hover:*:text-hover *:cursor-pointer">
+          <li className="text-2xl !p-0 hover:text-main transition">
             <RiMoonLine />
           </li>
-          <li className="text-2xl hover:text-main transition">
+          <li className="text-3xl hover:text-main transition">
             <AiOutlineAppstore onClick={() => setOpen(true)} />
             <Drawer
               anchor={"bottom"}
@@ -61,13 +61,13 @@ const MobileNavbar = () => {
               BackdropProps={{ invisible: true }}
             >
               <div className="p-6 pb-2 *:*:hover:text-dark-hover">
-                <div className="grid grid-cols-12 gap-5 *:cursor-pointer ">
+                <div className="grid grid-cols-12 gap-10 *:cursor-pointer ">
                   <span className={`col-span-4 pt-2 py-1 ${isScrolled ? "" : "active"}`}>
                     <ScrollLink to="home" offset={0}>
                       <div className="flex items-center justify-center">
                         <p className="flex flex-col items-center justify-center">
-                          <BiHomeAlt className="text-xl" />
-                          <span className="text-sm">Home</span>
+                          <BiHomeAlt className="text-2xl" />
+                          <span className="text-md">Home</span>
                         </p>
                       </div>
                     </ScrollLink>
@@ -77,8 +77,8 @@ const MobileNavbar = () => {
                     <ScrollLink to="about" offset={0}>
                       <div className="flex items-center justify-center">
                         <p className="flex flex-col items-center justify-center">
-                          <GoPerson className="text-xl drop-shadow-3xs drop-shadow-black" />
-                          <span className="text-sm">About</span>
+                          <GoPerson className="text-2xl drop-shadow-3xs drop-shadow-black" />
+                          <span className="text-md">About</span>
                         </p>
                       </div>
                     </ScrollLink>
@@ -88,36 +88,36 @@ const MobileNavbar = () => {
                     <ScrollLink to="skills" offset={40}>
                       <div className="flex items-center justify-center ">
                         <p className="flex flex-col items-center justify-center">
-                          <FiFileText className="text-lg" />
-                          <span className="text-sm">Skills</span>
+                          <FiFileText className="text-xl" />
+                          <span className="text-md">Skills</span>
                         </p>
                       </div>
                     </ScrollLink>
                   </span>
 
-                  <span className="col-span-6 pt-2 py-1">
+                  <span className="col-span-6 ms-10 pt-2 py-1">
                     <ScrollLink to="portfolio" offset={40}>
                       <div className="flex items-center justify-center ">
                         <p className="flex flex-col items-center justify-center">
-                          <IoImageOutline className="text-xl drop-shadow-6xs drop-shadow-black" />
-                          <span className="text-sm">Portfolio</span>
+                          <IoImageOutline className="text-2xl drop-shadow-6xs drop-shadow-black" />
+                          <span className="text-md">Portfolio</span>
                         </p>
                       </div>
                     </ScrollLink>
                   </span>
 
-                  <span className="col-span-6 pt-2 py-1">
+                  <span className="col-span-6 me-10 pt-2 py-1">
                     <ScrollLink to="contact" offset={50}>
                       <div className="flex items-center justify-center ">
                         <p className="flex flex-col items-center justify-center">
-                          <VscSend className="text-xl drop-shadow-4xs drop-shadow-black" />
-                          <span className="text-sm">Contact Me</span>
+                          <VscSend className="text-2xl drop-shadow-4xs drop-shadow-black" />
+                          <span className="text-md">Contact Me</span>
                         </p>
                       </div>
                     </ScrollLink>
                   </span>
                 </div>
-                <div className="flex justify-end text-2xl ms-2 w-full *:cursor-pointer">
+                <div className="flex justify-end text-4xl -ms-2 mb-4 w-full *:cursor-pointer">
                   <IoMdClose onClick={() => setOpen(false)} />
                 </div>
               </div>
