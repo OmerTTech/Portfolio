fe6a1fccb58e2e09c61a0c4805202c46d49fccc8
HyperOmerJ <omer.tapdiqov@gmail.com>
2025-09-03 23:02:02 +0400

    My first Portfolio published but this unfinished yet

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,17 +1,7 @@
 import React, { useState } from "react";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 import Button from "@mui/material/Button";
-import AIT_stats from "./image2.png";
-
-const portfolioSlides = [
-  {
-    id: 1,
-    image: AIT_stats,
-    title: "AIT stats",
-    text: "This project is a student-teacher management system built with React. It features secure role-based access using JWT and communicates with \n a JSON-Server mini backend via Axios.",
-    demoLink: "http://aitskills.netlify.app",
-  },
-];
+import portfolioSlides from "./PortfolioData.jsx";
 
 const Portfolio = () => {
   const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
---
+++ src/Components/Portfolio/PortfolioData.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AIT_stats from "./image2.png";
+
+export const portfolioSlides = [
+  {
+    id: 1,
+    image: AIT_stats,
+    title: "AIT stats",
+    text: "This project is a student-teacher management system built with React. It features secure role-based access using JWT and communicates with \n a JSON-Server mini backend via Axios.",
+    demoLink: "http://ait-stats.netlify.app",
+  },
+];
